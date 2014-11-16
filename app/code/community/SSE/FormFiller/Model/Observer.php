f5806c2f6f9be8be3d10180b814a6bafd7fcc0eb
Fabian Schmengler <fschmengler@sgh-it.eu>
2014-11-16 22:05:55 +0100

    Better theme compatibility for button markup; comments

--- app/code/community/SSE/FormFiller/Model/Observer.php
+++ app/code/community/SSE/FormFiller/Model/Observer.php
@@ -16,11 +16,9 @@
 class SSE_FormFiller_Model_Observer extends Mage_Core_Model_Abstract
 {
 
-// Schmengler Software Engineering Tag NEW_CONST
-
-// Schmengler Software Engineering Tag NEW_VAR
-
     /**
+     * Add form filler button to form if applicable
+     * 
      * @see event core_block_abstract_to_html_after
      * @return 
      */
@@ -33,8 +31,12 @@ class SSE_FormFiller_Model_Observer extends Mage_Core_Model_Abstract
         }
     }
 
-// Schmengler Software Engineering Tag NEW_METHOD
-
+    /**
+     * Returns true if form filler button should be added to given block
+     * 
+     * @param Mage_Core_Block_Abstract $block
+     * @return boolean
+     */
     protected function _shouldAddFormFiller(Mage_Core_Block_Abstract $block)
     {
     	if (! Mage::getStoreConfig('dev/formfiller/enabled') ||
@@ -49,18 +51,25 @@ class SSE_FormFiller_Model_Observer extends Mage_Core_Model_Abstract
     	}
     	return false;
     }
+    /**
+     * Returns block classes where form filler button can be added
+     * 
+     * @return string[]
+     */
     protected function _getSupportedBlocks()
     {
     	return array('Mage_Checkout_Block_Onepage_Billing', 'Mage_Checkout_Block_Onepage_Shipping');
     }
+    /**
+     * Returns HTML for form filler button
+     * 
+     * @return string
+     */
     protected function _getButtonHtml()
     {
-    	return '<button class="button button-formfiller" type="button" onclick="formFiller.fill(this.form)"><span>' .
-    		$this->_helper()->__('Fill form with random data') .
-    		'</span></button>';
-    }
-    protected function _helper()
-    {
-    	return Mage::helper('sse_formfiller');
+    	$helper = Mage::helper('sse_formfiller');
+        return '<button class="button btn btn-default button-formfiller" type="button" onclick="formFiller.fill(this.form)"><span><span>' .
+    		$helper->__('Fill form with random data') .
+    		'</span></span></button>';
     }
 }
